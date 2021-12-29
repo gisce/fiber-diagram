8b30f2bd3990356a3c8409b41faa62f3fbf15d39
Marc Güell Segarra <marc@ondori.dev>
2021-12-29 11:56:27 +0100

    Add Text to FiberUi

--- src/ui/FiberUi/FiberUi.tsx
+++ src/ui/FiberUi/FiberUi.tsx
@@ -1,6 +1,6 @@
 import { Fiber } from "base/Fiber";
 import React from "react";
-import { Rect } from "react-konva";
+import { Group, Rect, Text } from "react-konva";
 import { convertAttrUnitsToPixels } from "utils/pixelUtils";
 
 export const FiberUi = ({ fiber }: { fiber: Fiber }) => {
@@ -9,14 +9,26 @@ export const FiberUi = ({ fiber }: { fiber: Fiber }) => {
   const opts = convertAttrUnitsToPixels(attr);
 
   return (
-    <Rect
-      x={opts.position.x}
-      y={opts.position.y}
-      width={opts.size.width}
-      height={opts.size.height}
-      fill={color}
-      strokeWidth={1}
-      stroke="#555555"
-    />
+    <Group>
+      <Rect
+        x={opts.position.x}
+        y={opts.position.y}
+        width={opts.size.width}
+        height={opts.size.height}
+        fill={color}
+      />
+      <Text
+        text={`#${fiber.id}`}
+        x={opts.position.x}
+        y={opts.position.y}
+        width={opts.size.width * 2}
+        height={opts.size.height}
+        fontSize={10}
+        padding={opts.size.height}
+        fill="red"
+        strokeWidth={0.5}
+        stroke={"#000000"}
+      />
+    </Group>
   );
 };
