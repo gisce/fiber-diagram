07bfca54195e11b57674bed5e2f72c54450ca3ea
Marc Güell Segarra <marc@ondori.dev>
2022-01-03 09:31:22 +0100

    Remove invalid json storybook

--- src/ui/GridUi/GridUi.stories.tsx
+++ src/ui/GridUi/GridUi.stories.tsx
@@ -3,7 +3,6 @@ import { ComponentStory, ComponentMeta } from "@storybook/react";
 
 import { GridUi } from "./GridUi";
 import basic from "examples/basic.json";
-import three from "examples/three.json";
 import onelevel from "examples/onelevel.json";
 import twolevels from "examples/twolevels.json";
 import eight from "examples/8.json";
@@ -18,9 +17,6 @@ const Template: ComponentStory<typeof GridUi> = (args) => <GridUi {...args} />;
 
 export const Basic = Template.bind({});
 Basic.args = { inputJson: JSON.stringify(basic) };
-
-export const Three = Template.bind({});
-Three.args = { inputJson: JSON.stringify(three) };
 
 export const OneLevel = Template.bind({});
 OneLevel.args = { inputJson: JSON.stringify(onelevel) };
