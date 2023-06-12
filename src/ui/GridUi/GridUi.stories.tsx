7ab2e72b0723367a258c1ce7f06e95584e740702
Marc Güell Segarra <marc@ondori.dev>
2023-06-12 11:45:12 +0200

    Update stories

--- src/ui/GridUi/GridUi.stories.tsx
+++ src/ui/GridUi/GridUi.stories.tsx
@@ -1,40 +1,55 @@
 import React from "react";
-import { ComponentStory, ComponentMeta } from "@storybook/react";
+import type { Meta, StoryObj } from "@storybook/react";
 
 import { GridUi } from "./GridUi";
-import basic from "examples/basic.json";
-import eight from "examples/8.json";
-import nine from "examples/9.json";
-import ten from "examples/10.json";
-import simpleConnections from "examples/simpleConnections.json";
-import basic1Splitter from "examples/basic1Splitter.json";
-import basic2Splitter from "examples/basic2Splitter.json";
+import basic from "../../examples/basic.json";
+import eight from "../../examples/8.json";
+import nine from "../../examples/9.json";
+import ten from "../../examples/10.json";
+import simpleConnections from "../../examples/simpleConnections.json";
+import basic1Splitter from "../../examples/basic1Splitter.json";
+import basic2Splitter from "../../examples/basic2Splitter.json";
 import "antd/dist/antd.css";
 
-export default {
+const meta: Meta<typeof GridUi> = {
   title: "Components/GridUi",
   component: GridUi,
-} as ComponentMeta<typeof GridUi>;
+};
 
-const Template: ComponentStory<typeof GridUi> = (args) => <GridUi {...args} />;
+export default meta;
+type Story = StoryObj<typeof GridUi>;
 
-export const Basic = Template.bind({});
-Basic.args = { inputJson: JSON.stringify(basic) };
+// export default {
+//   title: "Components/GridUi",
+//   component: GridUi,
+// } as ComponentMeta<typeof GridUi>;
 
-export const Eight = Template.bind({});
-Eight.args = { inputJson: JSON.stringify(eight) };
+// const Template: ComponentStory<typeof GridUi> = (args) => <GridUi {...args} />;
 
-export const Nine = Template.bind({});
-Nine.args = { inputJson: JSON.stringify(nine) };
+export const Basic: Story = {
+  args: { inputJson: JSON.stringify(basic) },
+};
 
-export const Ten = Template.bind({});
-Ten.args = { inputJson: JSON.stringify(ten) };
+export const Eight: Story = {
+  args: { inputJson: JSON.stringify(eight) },
+};
 
-export const SimpleConnections = Template.bind({});
-SimpleConnections.args = { inputJson: JSON.stringify(simpleConnections) };
+export const Nine: Story = {
+  args: { inputJson: JSON.stringify(nine) },
+};
 
-export const BasicW1Splitter = Template.bind({});
-BasicW1Splitter.args = { inputJson: JSON.stringify(basic1Splitter) };
+export const Ten: Story = {
+  args: { inputJson: JSON.stringify(ten) },
+};
 
-export const BasicW2Splitter = Template.bind({});
-BasicW2Splitter.args = { inputJson: JSON.stringify(basic2Splitter) };
+export const SimpleConnections: Story = {
+  args: { inputJson: JSON.stringify(simpleConnections) },
+};
+
+export const BasicW1Splitter: Story = {
+  args: { inputJson: JSON.stringify(basic1Splitter) },
+};
+
+export const BasicW2Splitter: Story = {
+  args: { inputJson: JSON.stringify(basic2Splitter) },
+};
